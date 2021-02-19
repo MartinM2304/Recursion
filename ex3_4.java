715751a314b174678dbbcfaca3c11868b9890874
MartinM2304 <72139148+MartinM2304@users.noreply.github.com>
2021-02-19 10:23:04 +0200

    3

---
+++ ex3_4.java
@@ -0,0 +1,16 @@
+package tema3;
+
+public class ex3_4 {
+
+	public static void main(String[] args) {
+		
+		System.out.println( Array(0, new int[]{2, 5, 6}, 0));
+
+	}
+	public static double Array(int index,int n[], int result){
+		if (n.length-1==index)
+		return 0;
+		
+	}
+
+}
